d2096a7b6cfd6ac6e18a861f3ace227269e1ac57
Andy Wood <Andy@Andys-MacBook-Air.local>
2018-08-20 22:22:51 -0500

    Javascript update

--- Assets/Javascript/javascript.js
+++ Assets/Javascript/javascript.js
@@ -2,10 +2,21 @@
 
 $(document).ready(function () {
 
+    //Initialize Firebase database
+    var config = {
+        apiKey: "AIzaSyDc8YYktq7GywqfCv-g7Gbj4xioVQM29Mo",
+        authDomain: "recipe-assistant-15441.firebaseapp.com",
+        databaseURL: "https://recipe-assistant-15441.firebaseio.com",
+        projectId: "recipe-assistant-15441",
+        storageBucket: "recipe-assistant-15441.appspot.com",
+        messagingSenderId: "629989476926"
+    };
+    firebase.initializeApp(config);
+
     //When one of these buttons is selected, store the value into a variable to be used as a search parameter for the Big Oven API
-    var userChoice1; //set value to on(click) button selection
-    var userChoice2; //set value to on(click) button selection
-    var userChoice3; //set value to on(click) button selection
+    var userChoice1; //set value to radio button selection
+    var userChoice2; //set value to radio button selection
+    var userChoice3; //set value to radio button selection
     //Need to remove value on queryurl if button is deselected!
 
     var vegan = 'vgn=1';
@@ -14,25 +25,29 @@ $(document).ready(function () {
     var dairyFree = 'dyf=1';
     var searchTerms = 'include_ing=' + userChoice1, userChoice2, userChoice3;
 
-    //AJAX query & GET method for BigOven API
-    var searchBigOven = function (queryURL) {
-        apikeyBigOven = '&api_key=Ifu76FTQAzDd6CpFyGgVK9Y8IJ7MW196';
-        var queryURL = 'https://api2.bigoven.com/recipes?' + apikeyBigOven;
+    apikeyBigOven = '&api_key=Ifu76FTQAzDd6CpFyGgVK9Y8IJ7MW196';
+    var queryURL = 'https://api2.bigoven.com/recipes?' + apikeyBigOven;
 
-        //query search is defined based on the button on(clicks)
-        if ($('#vegan').on('click', function () {
-        }) === true) {
-            queryURL = 'https://api2.bigoven.com/recipes?' + vegan + searchTerms + apikeyBigOven;
-        } else if ($('#vegetarian').on('click', function () {
-        }) === true) {
-            queryURL = 'https://api2.bigoven.com/recipes?' + vegetarian + searchTerms + apikeyBigOven;
-        } else if ($('#glutenFree').on('click', function () {
-        }) === true) {
-            queryURL = 'https://api2.bigoven.com/recipes?' + glutenFree + searchTerms + apikeyBigOven;
-        } else if ($('#dairyFree').on('click', function () {
-        }) === true) {
-            queryURL = 'https://api2.bigoven.com/recipes?' + dairyFree + searchTerms + apikeyBigOven;
-        }
+    // query search is defined based on the button on(clicks)
+    $('#vegan').on('click', function () {
+        queryURL = 'https://api2.bigoven.com/recipes?' + vegan + apikeyBigOven;
+        console.log(queryURL);
+    })
+    $('#vegetarian').on('click', function () {
+        queryURL = 'https://api2.bigoven.com/recipes?' + vegetarian + apikeyBigOven;
+        console.log(queryURL);
+    })
+    $('#glutenFree').on('click', function () {
+        queryURL = 'https://api2.bigoven.com/recipes?' + glutenFree + apikeyBigOven;
+        console.log(queryURL);
+    })
+    $('#dairyFree').on('click', function () {
+        queryURL = 'https://api2.bigoven.com/recipes?' + dairyFree + apikeyBigOven;
+        console.log(queryURL);
+    })
+
+    //AJAX query & GET method for BigOven API
+    var searchBigOven = function () {
 
         $.ajax({
             url: queryURL,
@@ -56,8 +71,14 @@ $(document).ready(function () {
 
             //Display random recipe on screen
             $('#randomRecipe').append(response.Title);
-            $('#randomIngredients').append(response.Ingredients);
-            $('#randomDescription').append(response.Description);
+
+            //For loop to display ingredients from randomRecipe response
+            for (let index = 0; index < response.Ingredients.length; index++) {
+
+                $('#randomIngredients').append("<ol id='newList'></ol>");
+                $("#newList").append("<li>" + response.Ingredients[index].Name + "</li>");
+            }
+
             $('#randomInstructions').append(response.Instructions);
         });
     }
@@ -65,12 +86,7 @@ $(document).ready(function () {
     //Display the random recipe of the day on sceen
     randomRecipe();
 
-    $('#filterSubmit').on('click', function () {
-        searchBigOven();
-        console.log("hey there");
-    });
-
-    $('#ingredientsSubmit').on('click', function () {
+    $('#submit').on('click', function () {
         searchBigOven();
     });
 
