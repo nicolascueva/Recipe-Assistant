214e16b903359c79d7eb7dacedbfc469c577af46
Andy Wood <Andy@dhcp-10-102-133-51.wireless.northwestern.private>
2018-08-21 10:49:41 -0500

    Small update to queryURL

--- Assets/Javascript/javascript.js
+++ Assets/Javascript/javascript.js
@@ -2,20 +2,7 @@
 
 $(document).ready(function () {
 
-    //Initialize Firebase database
-    var config = {
-        apiKey: "AIzaSyDc8YYktq7GywqfCv-g7Gbj4xioVQM29Mo",
-        authDomain: "recipe-assistant-15441.firebaseapp.com",
-        databaseURL: "https://recipe-assistant-15441.firebaseio.com",
-        projectId: "recipe-assistant-15441",
-        storageBucket: "recipe-assistant-15441.appspot.com",
-        messagingSenderId: "629989476926"
-    };
-    firebase.initializeApp(config);
-
     const BIG_OVEN_KEY = 'Ifu76FTQAzDd6CpFyGgVK9Y8IJ7MW196';
-    
-    //When one of these buttons is selected, store the value into a variable to be used as a search parameter for the Big Oven API
 
     //AJAX query & GET method for BigOven API
     var searchBigOven = function (queryURL) {
@@ -58,18 +45,18 @@ $(document).ready(function () {
 
     $('#submit').on('click', function () {
         //stores value of ingredients for use in queryURL
-        var userChoice1 =  $( "#userChoice1 option:selected" ).text();
-        var userChoice2 =  $( "#userChoice2 option:selected" ).text();
-        var userChoice3 =  $( "#userChoice3 option:selected" ).text();
-        var searchTerms = userChoice1 + ',' +  userChoice2 + ',' + userChoice3;
-        
-        var queryURL = 'https://api2.bigoven.com/recipes?' + 
-            'api_key=' + BIG_OVEN_KEY + 
-            'include_ing' + searchTerms + 
+        var userChoice1 = $("#userChoice1 option:selected").text();
+        var userChoice2 = $("#userChoice2 option:selected").text();
+        var userChoice3 = $("#userChoice3 option:selected").text();
+        var searchTerms = userChoice1 + ',' + userChoice2 + ',' + userChoice3;
+
+        var queryURL = 'https://api2.bigoven.com/recipes?' +
+            'include_ing' + searchTerms +
             'vgn=' + $("#vegan").is(":checked") ? '1' : '0' +
-            'vtn=' + $("#vegetarian").is(":checked") ? '1' : '0' +
-            'glf=' + $("#glutenFree").is(":checked") ? '1' : '0' +
-            'dyf=' + $("#dairyFree").is(":checked") ? '1' : '0';
+                'vtn=' + $("#vegetarian").is(":checked") ? '1' : '0' +
+                    'glf=' + $("#glutenFree").is(":checked") ? '1' : '0' +
+                        'dyf=' + $("#dairyFree").is(":checked") ? '1' : '0' +
+                        'api_key=' + BIG_OVEN_KEY;
         searchBigOven(queryURL);
     });
 
